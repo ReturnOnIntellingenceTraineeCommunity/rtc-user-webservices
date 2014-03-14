a93725a18b67293fa13c86b92a7d8893394b4e5f
Bogdan <chernichenkobogdan@gmail.com>
2014-03-14 21:06:55 +0200

    New Files

---
+++ src/main/java/org/roi/rtc/webservice/user/ConfigurationClass.java
@@ -0,0 +1,29 @@
+package org.roi.rtc.webservice.user;
+
+/**
+ * Created by Chernichenko Bogdan on 14.03.14.
+ */
+
+import com.yammer.dropwizard.config.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class ConfigurationClass extends Configuration {
+    @NotEmpty
+    @JsonProperty
+    private String template;
+
+    @NotEmpty
+    @JsonProperty
+    private String defaultName = "Stranger";
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public String getDefaultName() {
+        return defaultName;
+    }
+}
+
+
---
+++ src/main/java/org/roi/rtc/webservice/user/core/SayHello.java
@@ -0,0 +1,22 @@
+package org.roi.rtc.webservice.user.core;
+
+/**
+ * Created by Chernichenko Bogdan on 14.03.14.
+ */
+public class SayHello {
+    private final long id;
+    private final String content;
+
+    public SayHello(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
