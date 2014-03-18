2eba2312ed8ad11e3c05d13d704871cfe1e0a209
Lapshin098 <lapshin098@gmail.com>
2014-03-18 17:08:06 +0200

    Connected Hibernate and Springframework

--- src/main/java/roi/rtc/webservices/user/MainService.java
+++ src/main/java/roi/rtc/webservices/user/MainService.java
@@ -8,6 +8,13 @@ import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
 import roi.rtc.webservices.user.core.ConfigurationClass;
 import roi.rtc.webservices.user.resources.Resource;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import java.io.File;
 
 public class MainService extends Service<ConfigurationClass> {
     public static void main(String[] args) throws Exception {
@@ -25,4 +32,25 @@ public class MainService extends Service<ConfigurationClass> {
 //        final String defaultName = configuration.getDefaultName();
         environment.addResource(new Resource());
     }
+
+    private static final SessionFactory sessionFactory
+            = configureSessionFactory();
+    private static ServiceRegistry serviceRegistry;
+
+
+    private static SessionFactory configureSessionFactory()
+            throws HibernateException {
+
+        File file = new File("src/main/java/roi.rtc.webservices.user/config/hibernate.cfg.xml");
+        Configuration configuration = new Configuration().configure(file);
+        serviceRegistry = new ServiceRegistryBuilder().applySettings(
+                configuration.getProperties()).buildServiceRegistry();
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
 }
