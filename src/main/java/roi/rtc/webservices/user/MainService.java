470716a7ea143e74e7d22516f2826e1e69bc8a60
Bogdan <chernichenkobogdan@gmail.com>
2014-03-20 13:26:52 +0200

    hybernate last ver

--- src/main/java/roi/rtc/webservices/user/MainService.java
+++ src/main/java/roi/rtc/webservices/user/MainService.java
@@ -3,55 +3,45 @@ package roi.rtc.webservices.user;
 /**
  * Created by Chernichenko Bogdan on 14.03.14.
  */
+import com.yammer.dropwizard.Bundle;
 import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.assets.AssetsBundle;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.db.DatabaseConfiguration;
+import com.yammer.dropwizard.hibernate.HibernateBundle;
+import roi.rtc.webservices.user.config.MainServiceConfiguration;
 import roi.rtc.webservices.user.core.ConfigurationClass;
-import roi.rtc.webservices.user.resources.UserResource;
-/*import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
-
-import java.io.File;*/
+import roi.rtc.webservices.user.entities.Dao1;
+import roi.rtc.webservices.user.resources.Resource;
 
 public class MainService extends Service<ConfigurationClass> {
+
     public static void main(String[] args) throws Exception {
         new MainService().run(args);
     }
 
+    private final HibernateBundle<MainServiceConfiguration> hibernate
+            = new HibernateBundle<MainServiceConfiguration>(Dao1.class) {
+
+                @Override
+                public DatabaseConfiguration getDatabaseConfiguration(MainServiceConfiguration configuration) {
+                    return configuration.getDatabase();
+                }
+            };
+
     @Override
     public void initialize(Bootstrap<ConfigurationClass> bootstrap) {
-        bootstrap.setName("hello-world");//users-web-service
+        bootstrap.setName("hello-world");
+        bootstrap.addBundle(new AssetsBundle("/assets/", "/"));
+        bootstrap.addBundle((Bundle) hibernate);
     }
 
     @Override
-    public void run(ConfigurationClass configuration,Environment environment) {
-        final String template = configuration.getTemplate();
+    public void run(ConfigurationClass configuration, Environment environment) {
+//        final String template = configuration.getTemplate();
 //        final String defaultName = configuration.getDefaultName();
-        environment.addResource(new UserResource());
-        environment.addHealthCheck(new TemplateHealthCheck(template));
+        environment.addResource(new Resource());
     }
-
-/*    private static final SessionFactory sessionFactory
-            = configureSessionFactory();
-    private static ServiceRegistry serviceRegistry;
-
-
-    private static SessionFactory configureSessionFactory()
-            throws HibernateException {
-
-        File file = new File("src/main/java/roi.rtc.webservices.user/config/hibernate.cfg.xml");
-        Configuration configuration = new Configuration().configure(file);
-        serviceRegistry = new ServiceRegistryBuilder().applySettings(
-                configuration.getProperties()).buildServiceRegistry();
-        return configuration.buildSessionFactory(serviceRegistry);
-    }
-
-
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }*/
 
 }
