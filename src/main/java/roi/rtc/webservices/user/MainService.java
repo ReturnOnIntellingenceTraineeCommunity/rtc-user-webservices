fe1212b1e519174090fb9b4dc07eeb65f8c33d88
Bogdan <chernichenkobogdan@gmail.com>
2014-03-20 13:42:16 +0200

    resurrecting master branch

--- src/main/java/roi/rtc/webservices/user/MainService.java
+++ src/main/java/roi/rtc/webservices/user/MainService.java
@@ -3,45 +3,34 @@ package roi.rtc.webservices.user;
 /**
  * Created by Chernichenko Bogdan on 14.03.14.
  */
-import com.yammer.dropwizard.Bundle;
 import com.yammer.dropwizard.Service;
-import com.yammer.dropwizard.assets.AssetsBundle;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
-import com.yammer.dropwizard.db.DatabaseConfiguration;
-import com.yammer.dropwizard.hibernate.HibernateBundle;
-import roi.rtc.webservices.user.config.MainServiceConfiguration;
 import roi.rtc.webservices.user.core.ConfigurationClass;
-import roi.rtc.webservices.user.entities.Dao1;
-import roi.rtc.webservices.user.resources.Resource;
+import roi.rtc.webservices.user.resources.UserResource;
+/*import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import java.io.File;*/
 
 public class MainService extends Service<ConfigurationClass> {
-
     public static void main(String[] args) throws Exception {
         new MainService().run(args);
     }
 
-    private final HibernateBundle<MainServiceConfiguration> hibernate
-            = new HibernateBundle<MainServiceConfiguration>(Dao1.class) {
-
-                @Override
-                public DatabaseConfiguration getDatabaseConfiguration(MainServiceConfiguration configuration) {
-                    return configuration.getDatabase();
-                }
-            };
-
     @Override
     public void initialize(Bootstrap<ConfigurationClass> bootstrap) {
-        bootstrap.setName("hello-world");
-        bootstrap.addBundle(new AssetsBundle("/assets/", "/"));
-        bootstrap.addBundle((Bundle) hibernate);
+        bootstrap.setName("hello-world");//users-web-service
     }
 
     @Override
-    public void run(ConfigurationClass configuration, Environment environment) {
-//        final String template = configuration.getTemplate();
+    public void run(ConfigurationClass configuration,Environment environment) {
+        final String template = configuration.getTemplate();
 //        final String defaultName = configuration.getDefaultName();
-        environment.addResource(new Resource());
+        environment.addResource(new UserResource());
+        environment.addHealthCheck(new TemplateHealthCheck(template));
     }
-
 }
